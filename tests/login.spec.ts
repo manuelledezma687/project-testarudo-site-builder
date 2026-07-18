3076a88c341d8252215111e0d6d286f16e27f746
Jenifer Pimentel <jeniferpimentel18@gmail.com>
2026-07-18 20:41:29 +0200

    adding workflow for github actions

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,46 +1,66 @@
-import { test } from '@playwright/test';
-import { LoginPage } from '../src/pages/login.page';
-import { DashboardPage } from '../src/pages/dashboard.page';
-import { Cookies } from '../src/components/cookiesModal/cookies';
-import { navBar } from '../src/components/navBar/navBar';
+import { expect, test } from '../src/fixtures/index';
 import users from '../src/test-data/users.json';
+
+const password = {
+  admin: process.env.ADMIN_PASSWORD!,
+  student: process.env.STUDENT_PASSWORD!,
+}
 
 
 test.describe('Login in Testarudo platform', () => {
 
-  let login: LoginPage;
-  let dashboard: DashboardPage;
-  let menu: navBar;
-  let cookies: Cookies;
 
-  test.beforeEach( async({page}) => {
-    login = new LoginPage(page);
-    cookies = new Cookies(page);
-    menu = new navBar(page);
-    dashboard = new DashboardPage(page);
+  test.beforeEach( async({page , cookies, menu}) => {
     await page.goto('');
     await cookies.clickAcceptCookies();
     await menu.clickLoginSection();
   });
 
-  test('Test login Admin', async () => {
-  await login.loginUser(users.adminUser.email,users.adminUser.password);
-  await dashboard.isAdminPanel();
+  test('Test login Admin', async ({ loginPage, dashboardPage } ) => {
+  await loginPage.loginUser(users.adminUser.email,password.admin);
+  await dashboardPage.isAdminPanel();
 });
 
-  test('Test login User', async () => {
-  await login.loginUser(users.StudentUser.email,users.StudentUser.password);
-  await dashboard.isStudentPanel();
+  test('Test login User', async ({ loginPage, dashboardPage } ) => {
+  await loginPage.loginUser(users.StudentUser.email,password.student);
+  await dashboardPage.isStudentPanel();
 });
 
-  test('Test login Admin URL', async () => {
-  await login.loginUser(users.adminUser.email,users.adminUser.password);
-  await dashboard.isAdminLogged();
+  test.only('Test login Admin URL', async ({ loginPage, dashboardPage,page } ) => {
+  await loginPage.loginUser(users.adminUser.email,password.admin);
+  await dashboardPage.isAdminLogged();
 });
 
-test('Test login User URL', async () => {
-  await login.loginUser(users.StudentUser.email,users.StudentUser.password);
-  await dashboard.isStudentLogged();
+test('Test login User URL', async ({ loginPage, dashboardPage } ) => {
+  await loginPage.loginUser(users.StudentUser.email,password.student);
+  await dashboardPage.isStudentLogged();
+});
+
+test('Test login Admin Contains Welcome message', async ({ loginPage, dashboardPage } ) => {
+  await loginPage.loginUser(users.adminUser.email,password.admin);
+  await dashboardPage.isAdminContainTheText("Panel de administración");
 });
 
 });
+
+test.describe('Login Snapshot', () => {
+
+test('Test login Snapshot HTML', async ({ page, cookies, menu } ) => {
+  await page.goto('');
+  await cookies.clickAcceptCookies();
+  await menu.clickLoginSection();
+  const form = page.locator('form').first();
+  const html = await form.innerHTML();
+  expect(html).toMatchSnapshot('loginpage.html');
+
+});
+
+test('Test login Snapshot PNG', async ({ page,cookies, menu } ) => {
+  await page.goto('');
+  await cookies.clickAcceptCookies();
+  await menu.clickLoginSection();
+  const submitButton = page.getByTestId('auth-submit');
+  await expect(submitButton).toHaveScreenshot('loginpage.png');
+});
+
+  });
